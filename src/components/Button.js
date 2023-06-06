0974618dd7f4e6fa1fc75541b1485a5080539d48
tamiresns <tamiresmn@live.com>
2023-06-06 13:18:07 -0300

    add o node_modules ao .gitgnore

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ label, onClick }) => {
   return (
-    <button type="submit" onClick={ onClick }>
+    <button type="submit" onClick={onClick}>
       { label } 
     </button>
   );
