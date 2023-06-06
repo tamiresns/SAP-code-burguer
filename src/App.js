9654400ce51c7e52ed59310ee77293f87c54c439
Bru-Silveira <brunasilveira_adm@outlook.com>
2023-06-06 15:21:46 -0300

    Logo Burger

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-    
+
       </Routes>
     </BrowserRouter>
   );
 };
-
+  
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,35 @@
+// import React from 'react';
+
+// function buttonPedidos() {
+//   const [total, setTotal] = React.useState(0);
+
+//   return (
+//     <button onClick = {() => setTotal(total +1)}>Novo Pedido {total}</button>
+//   )
+// }
+
+// function codeBurger() {
+//   return (
+//     <><div>
+//     <h1>codeBurger</h1>
+//     <img src="burger.png" alt="Imagem do burger"></img>
+//     </div><button type="submit" onClick={onClick}>
+//         {label}
+//       </button></>
+//   );
+// };
+
+
+// export default (buttonPedidos, codeBurger);
+
 import React from 'react';
 
-function buttonPedidos() {
-  const [total, setTotal] = React.useState(0);
-
+const Button = ({ label, onClick }) => {
   return (
-    <button onClick = {() => setTotal(total +1)}>Novo Pedido {total}</button>
-  )
-}
-
-function codeBurger() {
-  return (
-    <div>
-      <h1>codeBurger</h1>
-      <img src = "burger.png" alt = "Imagem do burger"></img>
-    </div>
+    <button type="submit" onClick={onClick}>
+      { label } 
+    </button>
   );
 };
 
-
-export default (buttonPedidos, codeBurger);
+export default Button;
