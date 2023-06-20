9883d9bc76907c3dbd5541be231b3046aa7da661
tamiresns <tamiresmn@live.com>
2023-06-20 13:52:22 -0300

    finalizando o header (logo, botao voltar e titulo)

--- src/pages/Cadastrar-pedido.js
+++ src/pages/Cadastrar-pedido.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Cadastrar-pedido.css';
 import ButtonMenuBreakfast from '../components/ButtonMenu.js';
 import ButtonMenuDia from '../components/ButtonMenu.js';
-
+import Header from '../components/Header.js'
 
 
 const CadastrarPedido = () => {
     return (
+      <div>
+        <Header/>
         <div className="menu-container">
           <h1 className="title-menu">Menu</h1>
           <div className="button-menu">
@@ -15,6 +17,7 @@ const CadastrarPedido = () => {
             
           </div>
         </div>
+      </div>
     )
 
 }
